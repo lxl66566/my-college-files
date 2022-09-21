884ebe4fb7ab091089aeccbfaef182257ad60cc1
lxl66566 <18259734087@163.com>
2022-09-21 22:59:50 +0800

    20220921-22:59:50

---
+++ 大学物理及其实验/随手写的算标准偏差.py
@@ -0,0 +1,9 @@
+from math import *
+# a = list(map(float,input().split(' ')))
+# avg = sum(a) / 6
+# b = []
+# for i in a:
+#     b.append(abs(i - avg) ** 2)
+# print(sqrt(sum(b) / 5))
+a = 0.014
+print(sqrt((1.11*a / sqrt(6)) ** 2) + (0.683 * 0.002) ** 2 )
