9fd1276fe33cfab921782d8ced5b81e843ffbeda
lxl66566 <18259734087@163.com>
2023-06-19 18:40:16 +0800

    20230619-18:40:15

---
+++ 信息科学与工程学院/数字系统设计/实践/last/cargo_sorter.v
@@ -0,0 +1,19 @@
+module cargo_sorter (
+    input [3:0] weight,
+    output reg light,
+    output reg medium_,
+    output reg heavy
+);
+
+    always @(weight) begin
+        light = 0;
+        medium_ = 0;
+        heavy = 0;
+        case (weight)
+            4'b0001, 4'b0010, 4'b0011: light = 1;
+            4'b0100, 4'b0101, 4'b0110: medium_ = 1;
+            default: heavy = 1;
+        endcase
+    end
+
+endmodule
---
+++ 信息科学与工程学院/数字系统设计/实践/last/simu.v
@@ -0,0 +1,34 @@
+module simu;
+
+reg clk;
+reg rst;
+reg [3:0] weight;
+wire light_out, medium_out, heavy_out;
+
+// Instantiate the top_module
+top_module tm (
+    .clk(clk),
+    .rst(rst),
+    .weight(weight),
+    .light_out(light_out),
+    .medium_out(medium_out),
+    .heavy_out(heavy_out)
+);
+
+initial begin
+    clk = 1'b0;
+    rst = 1'b1;
+    weight = 4'b0000;
+
+    // Test the top_module for different weights
+    #100 rst = 1'b0;  // Apply reset
+    #100 weight = 4'b0001;  // Test light weight
+    #100 weight = 4'b0100;  // Test medium weight
+    #100 weight = 4'b0111;  // Test heavy weight
+    #100 weight = 4'b1011;  // Test another heavy weight
+    #100 $finish;
+end
+
+always #5 clk = ~clk;  // Clock generation with a period of 10 time units
+
+endmodule
